01aa10217151c8c9bd54b2738ad2be048e7e73f9
melman <valentinmarto@gmail.com>
2018-03-16 11:07:24 -0300

    v2.1

---
+++ empleado.py
@@ -0,0 +1,16 @@
+class Empleado(object):
+    nombre=None
+    apellido=None
+    telefono=None
+    fecha_Nac=None
+    fecha_ingre=None
+
+    def __init__(self):
+        asist = [7]
+        cuando =[7]
+        porcentaje = []
+
+    def ingreso(self,dia):
+        self.asist.append(dia)
+
+    def chequeo(self,asist):
