a7e78e60f2adedb8b9c0e50387ac4ca3e0d396e6
melman <valentinmarto@gmail.com>
2018-03-21 16:50:21 -0300

    v2.1

--- empleado.py
+++ empleado.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Empleado(object):
     nombre=None
     apellido=None
@@ -6,11 +8,30 @@ class Empleado(object):
     fecha_ingre=None
 
     def __init__(self):
-        asist = [7]
-        cuando =[7]
+
+        fecha_dias = []
+        asist = []
+        cuando =[]
         porcentaje = []
+#weekday devuelve dia de la semana por numero empieza 0 lunes
+    def dia_ingrso(self,fecha):
+        self.fecha_ingreso=self.fecha_dias[0].days
 
     def ingreso(self,dia):
-        self.asist.append(dia)
+        self.asist += dia
 
-    def chequeo(self,asist):
+    def chequeo(self):
+        for item in self.asist
+            dia=item.weekday
+            if self.cuando[dia]==True
+                cant_asist+=1
+
+
+
+    def porcentaje(self):
+        porcen_total = 0
+        i=0
+        for item in self.porcentaje:
+            porcen_total += item
+            i+=1
+        return(porcen_total/i)
